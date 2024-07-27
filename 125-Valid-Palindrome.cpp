c294d746c8cc8e9e7be07b4f8aa213ed8207b65f
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-07-27 16:40:05 +0300

    [125] [Time Beats: 8.97%] [Memory Beats: 21.97%] - LeetPush

---
+++ 125-Valid-Palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = \\;
+        for (char c : s) {
+            if (isalnum(c)) {
+                str += tolower(c);
+            }
+        }
+
+        int l = 0, r = str.size() - 1;
+        while (l < r) {
+            if (str[l] != str[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
